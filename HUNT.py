bd37c7857b2258a92f274466569afb695936530d
GlebAnatolyevich <66731528+GlebAnatolyevich@users.noreply.github.com>
2020-06-12 11:13:02 +0300

    Revert "migrate to aiogram + new token"

--- HUNT.py
+++ HUNT.py
@@ -1,20 +1,12 @@
-import aiohttp
-from aiogram import Bot, Dispatcher
-from aiogram.utils import executor
-
 import config
-
-bot = Bot(token=config.token,
-          proxy='socks5://116.203.53.174:1008',
-          proxy_auth=aiohttp.BasicAuth(login='telegram',
-                                       password='telegram'))
-dp = Dispatcher(bot)
+import telebot
+from telebot import apihelper
+bot = telebot.TeleBot(config.token)
+apihelper.proxy = {'https':'https//95.110.194.245:54871'}
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message):
-    await bot.send_message(message.chat.id, "Привет, я искуственный интилект Илья!")
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Привет, я искуственный интилект Илья!")
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp)
